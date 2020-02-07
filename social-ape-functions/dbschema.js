c8cb6f9505b91220313093bc76679dc64bf2dc9a
Stephen Axtell <axtell.stephen@gmail.com>
2020-02-07 12:06:29 +0200

    converting to use express

---
+++ social-ape-functions/dbschema.js
@@ -0,0 +1,14 @@
+
+// File just for reference of what our db looks like
+
+let db = {
+	screams: [
+		{
+			userHandle: 'user',
+			body: 'This is the scream body',
+			createdAt: '2020-02-07T09:53:01.995Z', // ISO String format
+			likeCount: 5,
+			commentCount: 10
+		}
+	]
+}
--- social-ape-functions/functions/index.js
+++ social-ape-functions/functions/index.js
@@ -1,7 +1,9 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const express = require('express');
 
 admin.initializeApp();
+const app = express();
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -9,39 +11,47 @@ admin.initializeApp();
 
 /* Here using nodejs to store functions on firebase. THen we use end points url (like restful api) created in firebase to run them
 We can find them in the firebase dashboad. We needed to run firebase deploy to put them in firebase*/
+/* Now using express instead */
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-	response.send("Hello , whats cracking");
-});
 
-exports.getScreams = functions.https.onRequest((req, res) => {
-	admin.firestore().collection('screams').get()
-		.then(data => {
-			let screams = [];
-			data.forEach(doc => {
-				screams.push(doc.data());
+app.get('/screams', (req, res) => {
+	admin.firestore()
+	.collection('screams')
+	.orderBy('createdAt', 'desc')
+	.get()
+	.then(data => {
+		let screams = [];
+		data.forEach(doc => {
+			screams.push({
+				screamId: doc.id,
+				body: doc.data().body,
+				userHandle: doc.data().userHandle,
+				createdAt: doc.data().createdAt
 			});
-			return res.json(screams);
-		})
-		.catch(err => console.log(err));
+		});
+		return res.json(screams);
+	})
+	.catch(err => console.log(err));
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
+app.post('/scream', (req, res) => {
 
 	const newScream = {
 		body: req.body.body,
 		userHandle: req.body.userHandle,
-		createdAt: admin.firestore.Timestamp.fromDate(new Date())
+		createdAt: new Date().toISOString()
 	};
 
 	admin.firestore()
 		.collection('screams')
 		.add(newScream)
 		.then(doc => {
-			res.json({ message: `document ${doc.id} created successfully`});
+			return res.json({ message: `document ${doc.id} created successfully`});
 		})
 		.catch(err => {
 			res.status(500).json({ error: 'something went wrong'});
 			console.error(err);
 		});
 });
+
+exports.api = functions.https.onRequest(app); // registering app with routes
