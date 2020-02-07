756726aed1684286ea7ba401804ace0416e4e4f0
Stephen Axtell <axtell.stephen@gmail.com>
2020-02-07 16:01:07 +0200

    added end point to signup with email and password

--- social-ape-functions/functions/index.js
+++ social-ape-functions/functions/index.js
@@ -1,9 +1,29 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const express = require('express');
+const firebase = require('firebase');
+require('dotenv').config();
+
+
+const firebaseConfig = {
+	apiKey: process.env.APIKEY,
+	authDomain: process.env.AUTHDOMAIN,
+	databaseURL: process.env.DATABASEURL,
+	projectId: process.env.PROJECTID,
+	storageBucket: process.env.STORAGEBUCKET,
+	messagingSenderId: process.env.MESSAGINGSENDERID
+};
+
 
 admin.initializeApp();
+
+
+// using app
 const app = express();
+
+// Initializing firebase, so we can use firebase authorization
+firebase.initializeApp(firebaseConfig);
+
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -13,7 +33,29 @@ const app = express();
 We can find them in the firebase dashboad. We needed to run firebase deploy to put them in firebase*/
 /* Now using express instead */
 
+// Signup route
+app.post('/signup', (req, res) => {
+	const newUser = {
+		email: req.body.email,
+		password: req.body.password,
+		confirmPasword: req.body.confirmPasword,
+		handle: req.body.handle
+	};
 
+	// todo - validate
+
+	firebase.auth().createUserWithEmailAndPassword(newUser.email, newUser.password)
+		.then(data => {
+			return res.status(201).json({message: `user ${data.user.uid} signed up successfully`})
+		})
+		.catch((err) => {
+			console.error(err);
+			return res.status(500).json({ error: err.code});
+		});
+});
+
+
+// Get screams route
 app.get('/screams', (req, res) => {
 	admin.firestore()
 	.collection('screams')
@@ -29,11 +71,13 @@ app.get('/screams', (req, res) => {
 				createdAt: doc.data().createdAt
 			});
 		});
+		
 		return res.json(screams);
 	})
 	.catch(err => console.log(err));
 });
 
+// Add a scream route
 app.post('/scream', (req, res) => {
 
 	const newScream = {
@@ -41,6 +85,8 @@ app.post('/scream', (req, res) => {
 		userHandle: req.body.userHandle,
 		createdAt: new Date().toISOString()
 	};
+
+	
 
 	admin.firestore()
 		.collection('screams')
@@ -54,4 +100,4 @@ app.post('/scream', (req, res) => {
 		});
 });
 
-exports.api = functions.https.onRequest(app); // registering app with routes
+exports.api = functions.region('europe-west1').https.onRequest(app); // registering app with routes
