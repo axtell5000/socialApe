9d668dc23cc10c80b370aff1df36c69e85a00bac
Stephen Axtell <axtell.stephen@gmail.com>
2020-09-17 17:10:20 +0200

    moved over to using express. CHanged the firebase region to europe and added a schema for reference

--- social-ape-functions/functions/index.js
+++ social-ape-functions/functions/index.js
@@ -1,45 +1,53 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin')
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const express = require("express");
 
 admin.initializeApp();
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello world!");
-});
+const app = express();
 
-exports.getScreams = functions.https.onRequest((req, res) => {
-  admin.firestore().collection('screams').get()
-    .then(data => {
+app.get("/screams", (req, res) => {
+  admin
+    .firestore()
+    .collection("screams")
+    .orderBy("createdAt", "desc")
+    .get()
+    .then((data) => {
       let screams = [];
-      data.forEach(doc => {
-        screams.push(doc.data());
+      data.forEach((doc) => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
       });
       return res.json(screams);
     })
-    .catch((err) => console.error(err))
+    .catch((err) => console.error(err));
 });
 
-exports.createScream = functions.https.onRequest((req, res) => {
+app.post("/scream", (req, res) => {
   const newScream = {
     body: req.body.body,
     userHandle: req.body.userHandle,
-    createdAt: admin.firestore.Timestamp.fromDate(new Date())
+    createdAt: new Date().toISOString(),
   };
 
-  admin.firestore()
-    .collection('screams')
+  admin
+    .firestore()
+    .collection("screams")
     .add(newScream)
-    .then(doc => {
+    .then((doc) => {
       res.json({
-        message: `document ${doc.id} created successfully`
-      })
+        message: `document ${doc.id} created successfully`,
+      });
     })
-    .catch(err => {
+    .catch((err) => {
       res.status(500).json({
-        error: 'Something went wrong'
+        error: "Something went wrong",
       });
       console.error(err);
-    })
+    });
 });
+
+exports.api = functions.region('europe-west1').https.onRequest(app); // able to export multiple routes under /api, plus changing region to europe
---
+++ social-ape-functions/functions/schema.js
@@ -0,0 +1,9 @@
+let db = {
+  scream: [{
+    userHandle: 'user',
+    body: 'this is a scream',
+    createdAt: 'A date',
+    likeCount: 5,
+    commentCount: 10
+  }]
+}
