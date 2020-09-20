a2605dc3620b8aa6a359e86bec8c68cd4cb0a533
Stephen Axtell <axtell.stephen@gmail.com>
2020-09-20 11:36:08 +0200

    added signup route to backend, created user database document, added new user to database. Checks if email already exists

--- social-ape-functions/functions/index.js
+++ social-ape-functions/functions/index.js
@@ -1,14 +1,27 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const express = require("express");
+const firebase = require("firebase");
+
+const config = {
+  apiKey: "AIzaSyCs9WnDwOGMIHgxGi9LeLm4JKSCgoRfArk",
+  authDomain: "socialape-89327.firebaseapp.com",
+  databaseURL: "https://socialape-89327.firebaseio.com",
+  projectId: "socialape-89327",
+  storageBucket: "socialape-89327.appspot.com",
+  messagingSenderId: "959132455621",
+  appId: "1:959132455621:web:58f0781eac76f1fca916ac",
+  measurementId: "G-CD5QNQT7D6",
+};
 
 admin.initializeApp();
 const app = express();
+const DB = admin.firestore();
+
+firebase.initializeApp(config);
 
 app.get("/screams", (req, res) => {
-  admin
-    .firestore()
-    .collection("screams")
+  DB.collection("screams")
     .orderBy("createdAt", "desc")
     .get()
     .then((data) => {
@@ -33,9 +46,7 @@ app.post("/scream", (req, res) => {
     createdAt: new Date().toISOString(),
   };
 
-  admin
-    .firestore()
-    .collection("screams")
+  DB.collection("screams")
     .add(newScream)
     .then((doc) => {
       res.json({
@@ -50,4 +61,60 @@ app.post("/scream", (req, res) => {
     });
 });
 
-exports.api = functions.region('europe-west1').https.onRequest(app); // able to export multiple routes under /api, plus changing region to europe
+// Signup route
+app.post("/signup", (req, res) => {
+  const newUser = {
+    email: req.body.email,
+    password: req.body.password,
+    confirmPassword: req.body.confirmPassword,
+    handle: req.body.handle,
+  };
+
+  // TODO - validate
+
+  // Here we are checking if userhandle exist in database, cant have user with same handle
+  // if handle doesnt exist user can be created
+  let token, userId;
+  DB.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return res
+          .status(400)
+          .json({ handle: "This handle has already been taken" });
+      } else {
+        return firebase
+          .auth()
+          .createUserWithEmailAndPassword(newUser.email, newUser.password);
+      }
+    })
+    .then((data) => {
+      userId = data.user.uid;
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      token = idToken;
+      // to add users in our database not just under authentication
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId,
+      };
+      return DB.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
+      return res.status(201).json({ token });
+    })
+    .catch((err) => {
+      console.error(err);
+      if (err.code === "auth/email-already-in-use") {
+        return res.status(400).json({ email: "Email already in use" });
+      } else {
+        return res.status(500).json({
+          error: err.code,
+        });
+      }
+    });
+});
+exports.api = functions.region("europe-west1").https.onRequest(app); // able to export multiple routes under /api, plus changing region to europe
