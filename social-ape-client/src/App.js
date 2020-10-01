6e45149e37ca754f33b7aa4c93abd288bcacb8cb
Stephen Axtell <axtell.stephen@gmail.com>
2020-10-01 13:35:38 +0200

    initial files and fixes for front end and added Navbar using material

--- social-ape-client/src/App.js
+++ social-ape-client/src/App.js
@@ -1,65 +1,31 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { ThemeProvider as MuiThemeProvider } from '@material-ui/core/styles';
-import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
-import jwtDecode from 'jwt-decode';
-import axios from 'axios';
-
-// Redux
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import { SET_AUTHENTICATED } from './redux/types';
-
-//Components
-import NavBar from './components/Navbar';
-import themeObject from './util/theme';
-import AuthRoute from './util/AuthRoute';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // Pages
-import Home from './pages/home';
-import Login from './pages/login';
-import Signup from './pages/signup';
-import { logoutUser, getUserData } from './redux/actions/userActions';
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
 
-import './App.css';
+// Components
+import NavBar from "./components/Navbar";
 
-const theme = createMuiTheme(themeObject);
+import "./App.css";
 
-const token = localStorage.FBIdToken;
-
-if (token) {
-  const decodedToken = jwtDecode(token);
-	if (decodedToken.exp * 1000 < Date.now()) {
-		store.dispatch(logoutUser());   
-		window.location.href = '/login';	
-  } else {    
-		store.dispatch({ type: SET_AUTHENTICATED });
-		axios.defaults.headers.common['Authorization'] = token;
-		store.dispatch(getUserData());
-  }
-}
-
-class App extends Component {
-
-  render() {
-    return (
-			<MuiThemeProvider theme={theme}>
-				<Provider store={store}>					
-					<BrowserRouter>
-						<NavBar />
-						<div className="container">
-							<Switch>
-								<Route exact path="/" component={Home} />
-								<AuthRoute exact path="/login" component={Login} />
-								<AuthRoute exact path="/signup" component={Signup} />
-							</Switch>
-						</div>
-					</BrowserRouter>				
-				</Provider>
-      </MuiThemeProvider>
-    );
-  }
-
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <NavBar />
+        <div className="container">
+          <Switch>
+            <Route path="/" exact component={home} />
+            <Route path="/login" exact component={login} />
+            <Route path="/signup" exact component={signup} />
+          </Switch>
+        </div>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
--- social-ape-client/src/pages/home.js
+++ social-ape-client/src/pages/home.js
@@ -1,46 +1,9 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
 
-// Material stuff
-import Grid from '@material-ui/core/Grid';
-
-import Scream from '../components/Scream';
-import Profile from '../components/profile/Profile';
-
-class Home extends Component {
-
-	state = {
-		screams: null
-	};
-
-	componentDidMount() {
-		axios.get('/screams')
-			.then(res => {
-				console.log(res.data);
-				this.setState({
-					screams: res.data
-				})
-			})
-			.catch(err => console.log(err));
-	}
-
-	render() {
-		
-		let recentScreamsMarkup = this.state.screams ? (
-			this.state.screams.map(scream => <Scream key={scream.screamId} scream={scream} />)
-		) : <p>Loading...</p>;		
-
-		return (
-			<Grid container spacing={4}>
-				<Grid item sm={8} xs={12}>
-					{ recentScreamsMarkup }
-				</Grid>
-				<Grid item sm={4} xs={12}>
-					<Profile />
-				</Grid>
-			</Grid>
-		);
-	}
+class home extends Component {
+  render() {
+    return <div>Home page</div>;
+  }
 }
 
-export default Home;
+export default home;
